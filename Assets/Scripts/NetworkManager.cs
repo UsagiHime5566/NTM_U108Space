2492a70b19f32cdf53d4a7031a94c39c6fc9e5d8
Watane Kyua <99834141+watanekyua@users.noreply.github.com>
2022-03-02 02:49:49 +0800

    Reduice Log content

--- Assets/Scripts/NetworkManager.cs
+++ Assets/Scripts/NetworkManager.cs
@@ -102,7 +102,7 @@ public class NetworkManager : HimeLib.SingletonMono<NetworkManager>
         if (request.isNetworkError || request.isHttpError){
             Debug.Log("Network error has occured: " + request.GetResponseHeader(""));
         } else {
-            Debug.Log("Success: " + request.downloadHandler.text);
+            Debug.Log("Success: " + url);
             
             callback?.Invoke(request.downloadHandler.text);
         }
